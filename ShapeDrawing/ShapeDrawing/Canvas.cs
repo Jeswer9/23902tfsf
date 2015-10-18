51f9734adf9605411ecffd14df9000b0ff17ae4e
Jeswer9 <negen_jewer@live.nl>
2015-10-18 16:39:15 +0200

    Canvas

---
+++ ShapeDrawing/ShapeDrawing/Canvas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShapeDrawing
+{
+    public abstract class Canvas
+    {
+    }
+
+    class SVG_Canvas: Canvas
+    {
+
+    }
+
+    class CSharp_Canvas : Canvas
+    {
+
+    }
+}
